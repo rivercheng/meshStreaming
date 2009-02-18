168d40beb65a73663f329127ce87b15e309c8bf9
ransidesilva <ransidesilva@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-02-18 09:06:04 +0000

    trace file analysis file

---
+++ analyse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import string
+import math
+import datetime
+import sys
+import os
+
+input = 'DEFAULT.packet.2009.02.17.21.29.49.txt'
+inputFile = open(input)
+datarate = {}
+for line in inputFile.readlines():
+    fields = line.split(' ')
+    if fields[2] == 'R':
+       sec = int(fields[0])
+       if datarate.get(sec) == None:
+          datarate[sec] = int(fields[4])
+       else:
+          datarate[sec] += int(fields[4])
+        
+inputFile.close()
+
+print "time","datarate"
+for key in datarate.keys():
+        print key,datarate[key]
+
